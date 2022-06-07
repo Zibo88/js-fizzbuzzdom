2465888e4b737ceafec4bbb6605ceea3f0f6aa5b
Zibo88 <vincenzo.tardino1988@gmail.com>
2022-06-07 16:01:09 +0200

    modifica a milestone 2

--- milestone-2/js/script.js
+++ milestone-2/js/script.js
@@ -3,21 +3,21 @@
 //corretta (seguendo le regole della Milestone 1).
 
 
-
+const numberContainer = document.querySelector ('.number-container');
+const listItem = `<div class="box"></div>`;
 for(i = 1; i <= 100; i++){
     let myNumber = i;
     if (i % 5 === 0) {
     myNumber += ' Buzz';
-    console.log(myNumber);
     } else if (i % 3 === 0) {
         myNumber += ' Fizz';
-        console.log(myNumber);
     }
-    const numbersContainer = document.querySelector ('.numbers-container');
-    const listItem = `<div class="box"></div>`;
-    numbersContainer.innerHTML += listItem + myNumber;
-    console.log(numbersContainer);
+    numberContainer.innerHTML += listItem + myNumber; 
 }
+
+
+
+
 
 
 
---
+++ milestone-3/js/script.js
@@ -0,0 +1,24 @@
+//Per ogni numero, dato un container nel DOM, appendi un elemento 
+//html con il numero o la stringa corretta.
+//Applica uno stile differente a seconda del valore dell'indice 
+//per i multipli di 3, per i multipli di 5 e per i valori che sono sia 
+//multipli di 3 che di 5 (seguendo le regole della Milestone 1).
+//Per questa Milestone avete lo screenshot allegato al messaggio.
+//Prima di partire a scrivere codice poniamoci qualche domanda:
+//Come faccio a sapere se un numero è divisibile per un altro? 
+//Abbiamo visto qualcosa di particolare che possiamo usare? 
+//Come creare nuovi elementi html e appenderli al container?
+//Consigli del giorno:
+//1. scriviamo sempre prima dei commenti in italiano per capire cosa 
+//vogliamo fare
+//2. proviamo ad immaginare le operazioni 
+//che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+
+const numberContainer = document.querySelector('.number-container');
+console.log(numbersContainer);
+for(let i = 0; i <= 100; i ++){
+    let colorSquare = document.querySelector('.bcg-square');
+    console.log(colorSquare);
+}
+
